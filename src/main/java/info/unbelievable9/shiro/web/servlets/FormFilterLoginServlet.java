87f46a87fc81332d15153bfbf65b1a7b0accde0f
Unbelievable-Reborn <286932903@qq.com>
2018-07-27 11:57:32 +0800

    Add Basic/Form Filter Demo.

--- src/main/java/info/unbelievable9/shiro/web/servlets/FormFilterLoginServlet.java
+++ src/main/java/info/unbelievable9/shiro/web/servlets/FormFilterLoginServlet.java
@@ -19,6 +19,7 @@ public class FormFilterLoginServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        // If FormAuthenticationFilter Failed
         doPost(req, resp);
     }
 
